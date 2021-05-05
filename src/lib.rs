d21dc19a4ab44d3eca497097ccb57e02edb4a5b5
Magic Len <len@magiclen.org>
2021-05-05 14:05:49 +0800

    update doc

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ println!(directory_relative_path!("src"));
 println!(not_directory_relative_path!("Cargo.toml"));
 println!(file_relative_path!("Cargo.toml"));
 
-// The `tuple` feature let these macros support to input nested literal string tuples, which is useful when you want to use these macros inside a `macro_rules!` macro and concatenate with other literal strings.
+// The `tuple` feature allows these macros to support inputting nested literal string tuples, which is useful when you want to use these macros inside a `macro_rules!` macro and concatenate with other literal strings.
 // `$x:expr` matchers can be used in these macros thus.
 #[cfg(feature = "tuple")]
 {
